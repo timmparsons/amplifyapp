457041d0d1c29f1d7f65726eafafb37323e27e6e
Tim Parsons <timmparsons85@gmail.com>
2021-05-31 11:38:13 -0700

    added graphql api

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "us-east-2",
     "aws_user_pools_id": "us-east-2_ziS5XgkJF",
     "aws_user_pools_web_client_id": "5auj9i05d96kj0jep1diclo3tt",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://x25yhzwhfbdhbdep77nm2obc7i.appsync-api.us-east-2.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-q3cgjxmzrzbufm7wy2shdmzk6e"
 };
 
 
